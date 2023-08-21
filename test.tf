cb0950e2733ecbc123574d809a9c0eacbfc5c62a
rayen khalfaoui <92087882+rayenkhw@users.noreply.github.com>
2023-08-21 11:21:25 +0200

    Update test.tf

--- test.tf
+++ test.tf
@@ -27,8 +27,8 @@ data "vsphere_virtual_machine" "template" {
   datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
-resource "vsphere_virtual_machine" "terraformtest" {
-  name             = "terraformtest"
+resource "vsphere_virtual_machine" "testrayen" {
+  name             = "testrayen"
   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
   folder = "rayen"
   datastore_id     = data.vsphere_datastore.datastore.id
